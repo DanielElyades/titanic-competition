2ad629b2370a2296befc52fca01f3f12046acbd8
Daniel Elyades <danielelyades@gmail.com>
2025-12-07 14:23:45 -0300

    Aprendendo como plotar gráficos

--- titanic_train.py
+++ titanic_train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 train_data = pd.read_csv('train.csv')
 
@@ -11,7 +12,7 @@ pd.set_option('display.width', None)
 
 print(train_data.groupby('Sex')['Survived'].mean())
 print(train_data.groupby('Pclass')['Survived'].mean())
-
+print(train_data.groupby('Embarked')['Survived'].mean())
 
 #print(train_data['Sex'].value_counts())
 #print(train_data['Sex'].value_counts(normalize=True))
@@ -19,5 +20,8 @@ print(train_data.groupby('Pclass')['Survived'].mean())
 #print(train_data[train_data['Survived']==1])
 print(train_data.head())
 #print(train_data[train_data['Survived']==0])
-print(train_data.describe())
+#print(train_data.describe())
 print(train_data.isnull().sum())
+train_data['Age'].hist()
+train_data['Fare'].hist()
+plt.show()
