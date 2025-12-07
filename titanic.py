6718840649f84c1e9f9ac40faaec22d506c3a04f
Daniel Elyades <danielelyades@gmail.com>
2025-12-07 10:53:20 -0300

    Começando a entender o problema

--- titanic.py
+++ titanic.py
@@ -9,6 +9,15 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.width', None)
 
+print(train_data.groupby('Sex')['Survived'].mean())
+print(train_data.groupby('Pclass')['Survived'].mean())
+
+
+#print(train_data['Sex'].value_counts())
+#print(train_data['Sex'].value_counts(normalize=True))
+#print(train_data.groupby('SibSp')['Survived'].mean())
 #print(train_data[train_data['Survived']==1])
-#print(train_data)
-print(train_data[train_data['Survived']==0])
+print(train_data.head())
+#print(train_data[train_data['Survived']==0])
+print(train_data.describe())
+print(train_data.isnull().sum())
